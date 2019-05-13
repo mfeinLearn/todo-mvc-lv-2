aba75c1c5669d80b73f3396f7fa769c2eb04f8e7
github username <github email address>
2019-05-13 14:31:09 -0400

    Sprint 1: Create and Retrieve and List List

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,6 +1,31 @@
 class ListsController < ApplicationController
   def index
+    @list = List.new
     @lists = List.all
     # render 'lists/index.html.erb'
   end
+
+  def show
+    # I need to load the list
+    # Which list?
+
+    params # have all the data passed by a user
+                  #   /lists/:id
+    @list = List.find(params[:id])
+  end
+
+  def create
+    @list = List.new(list_params)
+    @list.name = params[:list][:name]
+    @list.save
+
+    redirect_to list_url(@list)
+  end
+
+  private
+
+  def list_params
+    params.require(:list).permit(:name)
+  end
+
 end
