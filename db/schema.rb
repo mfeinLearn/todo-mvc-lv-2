84b83d05023f7f40a812c3128b17a7e28c26dd21
github username <github email address>
2019-05-14 16:02:42 -0400

    sprint 3 - part 1

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_13_185112) do
+ActiveRecord::Schema.define(version: 2019_05_14_195802) do
 
   create_table "items", force: :cascade do |t|
     t.string "description"
     t.integer "list_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "status"
   end
 
   create_table "lists", force: :cascade do |t|
